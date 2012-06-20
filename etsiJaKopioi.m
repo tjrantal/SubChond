0176044473da1a81b197d3dbb9ec1b0b76506d53
tjrantal <tjrantal@gmail.com>
2012-06-20 12:05:56 +0300

    Finalized selecting proper data script etsiJaKoioi

--- etsiJaKopioi.m
+++ etsiJaKopioi.m
@@ -1,36 +1,21 @@
-close all;
 clear all;
+close all;
 clc;
-% juuri = 'Data';
-% kohde = 'karsittu\kh1';
-% juuri = 'Data\11013012\';
-% kohde = 'karsittu\kh2';
-juuri = 'Data\10070519\';
-kohde = 'karsittu\kh3';
+juuri = 'Data\';
+kohde = 'karsittu\kh';
 
 
-tulos = etsiData(juuri);
-tulos2 = kopioiData(tulos,kohde);
+hakemistot = dir(juuri);
+for i = 3:length(hakemistot)
+    tulos = etsiData([juuri hakemistot(i).name '\']);
+    if isdir([kohde num2str(i-2)])
+       disp(['Data exists!!! Nothgin done to ' kohde num2str(i-2)])
+    else
+        mkdir([kohde num2str(i-2)]);
+        tulos2 = kopioiData(tulos,[kohde num2str(i-2)]);
+    end
+end
 
-% for i = 1:length(tulos2)
-%    temp(i).SliceLocation = tulos2(i).info.SliceLocation;
-% end
-% [values,jarjestys] = sort([temp().SliceLocation]);
-% esa = figure;
-% for i = 1:length(tulos2)
-%    %tulos2(i).info.Width
-% %    [num2str(i) tulos2(i).info.SequenceName]
-% %    [num2str(i) ' ' tulos2(i).info.ProtocolName]
-% %    [num2str(i) ' ' tulos2(i).info.Private_0019_1008]
-% %    [num2str(i) ' ' num2str(tulos2(i).info.SeriesNumber)]
-% %    [num2str(i) ' ' num2str(tulos2(i).info.AcquisitionNumber)]
-% %     [num2str(i) ' ' num2str(tulos2(i).info.SliceLocation)]
-%    
-%    data = dicomread(tulos2(jarjestys(i)).info);
-%    imshow(mat2gray(data))
-%    drawnow;
-%     pause(0.5);
-% end
-% 
 % %tulos2(i).info.SpacingBetweenSlices    %3.6 mm
 % %tulos2(i).info.PixelSpacing    %0,2734 mm
+
